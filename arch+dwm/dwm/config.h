ad4f581a8b84149e7146c64111874e1bd3792192
User <user@example.com>
2021-07-19 17:30:41 +0300

    add default tag

--- arch+dwm/dwm/config.h
+++ arch+dwm/dwm/config.h
@@ -9,7 +9,7 @@ static const unsigned int gappih    = 5;       /* horiz inner gap between window
 static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "xos4 Terminus:size=10" };
@@ -44,6 +44,7 @@ static const Rule rules[] = {
 	{ "TelegramDesktop",    NULL,       NULL,       1 << 5,       0,           -1 },
 	{ "discord",    	NULL,       NULL,       1 << 5,       0,           -1 },
 	{ "Pcmanfm",  		NULL,	    NULL,       1 << 6,       0,           -1 },
+	{ "KeePassXC",  	NULL,	    NULL,       1 << 7,       0,           -1 },
 //	{ "Firefox",  		NULL, 	    NULL,       1 << 8,       0,           -1 },
 };
 
